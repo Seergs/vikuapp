238a112961fba4e745a7d30d4723b5624baa0e1e
Sergio Suarez <s.suarerez@gmail.com>
2026-09-08 12:42:17 -0600

    build(packages): raise macOS deployment floor to 14 for Observation

--- Packages/VikuAuth/Package.swift
+++ Packages/VikuAuth/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VikuAuth",
-    platforms: [.iOS(.v17), .macOS(.v13)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(name: "VikuAuth", targets: ["VikuAuth"]),
     ],
--- Packages/VikunjaCore/Package.swift
+++ Packages/VikunjaCore/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VikunjaCore",
-    platforms: [.iOS(.v17), .macOS(.v13)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(name: "VikunjaCore", targets: ["VikunjaCore"]),
     ],
