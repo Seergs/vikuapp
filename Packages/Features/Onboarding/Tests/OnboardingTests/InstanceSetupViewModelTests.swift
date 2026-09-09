12f34dde618a167ccaff50f123fa0dd10194b326
Sergio Suarez <s.suarerez@gmail.com>
2026-09-09 11:10:20 -0600

    test(onboarding): update insecure-url copy to canonical displayMessage

--- Packages/Features/Onboarding/Tests/OnboardingTests/InstanceSetupViewModelTests.swift
+++ Packages/Features/Onboarding/Tests/OnboardingTests/InstanceSetupViewModelTests.swift
@@ -87,9 +87,7 @@ struct InstanceSetupViewModelTests {
 
         await viewModel.saveConnection()
 
-        #expect(viewModel.validationState == .failure(
-            "That address uses http. Turn on \"Allow insecure connection\" to connect over an unencrypted link.",
-        ))
+        #expect(viewModel.validationState == .failure("That address uses an insecure http connection."))
         #expect(accountStore.accounts.isEmpty)
     }
 
